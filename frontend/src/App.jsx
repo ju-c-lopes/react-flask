66ed34101ed28b9e021e9a93f0982840dc2dc694
Juliano <juliano.co.lopes@gmail.com>
2025-04-06 12:13:43 -0300

    feat: add Counter component to track click counts to learn React Hook Usestate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import React, { useState } from 'react';
 import SearchSpeaker from './SearchSpeaker/SearchSpeaker.jsx';
 
 function SpeakerProfile(props) {
@@ -8,6 +9,17 @@ function SpeakerProfile(props) {
       <p>Position: {props.position}</p>
       <p>Company: {props.company}</p>
     </>
+  )
+}
+
+function Counter(props) {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>{props.name} has been clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Grow me up</button>
+    </div>
   )
 }
 
@@ -22,6 +34,8 @@ function App() {
         company="Tech Corp"
       />
       <SearchSpeaker />
+      <h2>Counter:</h2>
+      <Counter name="Counter" />
     </>
   )
 }
